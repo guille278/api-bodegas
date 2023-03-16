3a41556a9fa2f3671d7e616d7b9e30312f84ce4c
Christopher Barba <christopher@itree.mx>
2023-03-15 18:54:30 -0600

    Actualización api para el registro

--- app/Http/Controllers/api/AuthController.php
+++ app/Http/Controllers/api/AuthController.php
@@ -8,6 +8,7 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
